ca99d705fb8136b604677ca4dd52c860427a0462
George Webb <george.webb@oxbotica.com>
2020-07-16 10:50:56 +0100

    code challenge

---
+++ main.py
@@ -0,0 +1,40 @@
+# Graduate Coding Challenge - Python
+
+# Here are a series of small coding challeneges in cpp for you to try!
+# There is no right or wrong answer, just give it a go!
+
+# Don't worry about not finishing it.
+# Don't google a solution to the questions!
+# You can search for small things like "what does this function return".
+
+# Leave all the code you write, even the debug stuff :)!
+# Just comment it out if you need to run the app.
+
+# Each question has a URL to an explanation of what we are asking :)
+
+# Python -> You shouldn't need to import anything but you can import
+# standard libray stuff if you need.
+# Replit -> You can edit what happens when you hit run in the .replit file
+
+# Have fun!
+
+import unordered_numbers as data
+
+# -----------------------------------------------------------------------------
+
+# 1. Write a function that takes in a vector of unordered numbers
+#    (unordered_numbers.h) and returns a vectors of ordered numbers 
+#    (ascending order) using a merge sort. 
+#    DON'T USE std::sort!! (that makes it too easy)
+# Merge Sort: https://www.bbc.co.uk/bitesize/guides/zjdkw6f/revision/5
+
+# 2. Write a function to show your sorting works on your ordered numbers
+#    (e.g. Prints "Passed" to the terminal on an expected result).
+
+# 3. Write a function that performs a binary search to return 
+#    the index of a given number (if it exists!).
+# Binary Search: https://www.bbc.co.uk/bitesize/guides/zjdkw6f/revision/3
+# (Try not to read the sudo code at the end of the page....)
+
+# 4. Write a function to show your binary search works 
+#    (e.g. Prints "Passed" to the terminal on an expected result).
